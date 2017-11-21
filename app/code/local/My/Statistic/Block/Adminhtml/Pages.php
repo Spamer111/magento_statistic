b6e4687d8bd693c0a970877e1f2b48d12a9ba24c
Spamer111 <spam.by.86@gmail.com>
2017-11-21 14:42:34 +0300

    fixed block

--- app/code/local/My/Statistic/Block/Adminhtml/Pages.php
+++ app/code/local/My/Statistic/Block/Adminhtml/Pages.php
@@ -1,115 +1,125 @@
 <?php
 class My_Statistic_Block_Adminhtml_Pages extends Mage_Adminhtml_Block_Template{
 
-    protected $data = '';
-    protected $legend = '';
-    protected $rgba = '';
-    protected $countryDataMap = '';
-    protected $dataYearGist = array();
-    //protected $myYear = array(); // ?
-    protected $monthLegend= array();
     protected $month=array(1 => 'Janu', 2 => 'Feb', 3 => 'Mar',
                            4=> 'Apr', 5 => 'May', 6 => 'Jun',
                            7 => 'Jul', 8 => 'Aug', 9 => 'Sep',
                            10 => 'Oct', 11 => 'Nov', 12 => 'Dec');
-    protected $dayLegend = '';
-    protected $dayData = array();
-
     
-    protected function PieChart($collection,$chart){
-        $collection_chart = $collection->getCollection();// Получаем колекцию
-        $all_record = array();
-        foreach ($collection_chart as $v){
-            $all_record[]=$v[$chart]; // Получаем список всех записей
+    protected function pieChart($collection,$chart){
+        $arrayOfParameters = array();
+        $data = '';
+        $legend = '';
+        $rgba = '';
+        $countryDataMap = '';
+        $collectionChart = $collection->getCollection();// Получаем колекцию
+        $allRecord = array();
+        foreach ($collectionChart as $v){
+            $allRecord[]=$v[$chart]; // Получаем список всех записей
         }
-        $unique_record=array_unique($all_record); // уникальные записи
-        $quantity_unique_record = array(); // количество каждой уникальной записи в бд
-        foreach ($unique_record as $v){
-            $quantity_unique_record[] = count($collection->getCollection()-> addFieldToFilter($chart,array('eq'=>$v)));
+        $uniqueRecord=array_unique($allRecord); // уникальные записи
+        $quantityUniqueRecord = array(); // количество каждой уникальной записи в бд
+        foreach ($uniqueRecord as $v){
+            $quantityUniqueRecord[] = count($collection->getCollection()-> addFieldToFilter($chart,array('eq'=>$v)));
         }
-        $all_quantity = count($collection_chart); // считаем сколько всего записей -100%
-        $percent_unique_record = array(); // % уникальных записей от общего числа
-        foreach ($quantity_unique_record as $v) {
-            $percent_unique_record[] = round($v/$all_quantity*100,2);
+        $allQuantity = count($collectionChart); // считаем сколько всего записей -100%
+        $percentUniqueRecord = array(); // % уникальных записей от общего числа
+        foreach ($quantityUniqueRecord as $v) {
+            $percentUniqueRecord[] = round($v/$allQuantity*100,2);
         }
-        $this->data = implode(",",$percent_unique_record); // строка с % для заполнения диаграммы
-        $this->legend = "'".implode("','",$unique_record)."'"; // строка с легендой
-        foreach ($unique_record as $v) {
-            $this->rgba = "'rgba(".rand(0,255).",".rand(0,255).",".rand(0,255).",0.5)',".$this->rgba;// строка с цветовой заливкой
+        $data = implode(",",$percentUniqueRecord); // строка с % для заполнения диаграммы
+        $arrayOfParameters['data'] = $data;
+        $legend = "'".implode("','",$uniqueRecord)."'"; // строка с легендой
+        $arrayOfParameters['legend'] = $legend;
+        foreach ($uniqueRecord as $v) {
+            $rgba = "'rgba(".rand(0,255).",".rand(0,255).",".rand(0,255).",0.5)',".$rgba;// строка с цветовой заливкой
         }
+        $arrayOfParameters['rgba'] = $rgba;
         if($chart == 'country_name_en'){
-            $country_key = array();
-            foreach ($unique_record as $value){
-                $country_key[] = $value;
+            $countryKey = array();
+            foreach ($uniqueRecord as $value){
+                $countryKey[] = $value;
             }
-            foreach ($country_key as $key => $value) {
-                $this->countryDataMap = "['".$value."','".$value.": ".$quantity_unique_record[$key]."'],".$this->countryDataMap;
+            foreach ($countryKey as $key => $value) {
+                $countryDataMap = "['".$value."','".$value.": ".$quantityUniqueRecord[$key]."'],".$countryDataMap;
             }
         }
+        $arrayOfParameters['countryDataMap'] = $countryDataMap;
+        return $arrayOfParameters;
     }
 
-    protected function HistogramByMonth($collection){
-        $this->monthLegend = "'".implode("','",$this->month)."'"; // получаем строку из массива
-        $my_year = array();// массив с годами
-        if(array_key_exists('my_year',$_POST)){ // если существуе в $_POST значение my_year
-            foreach ($_POST['my_year'] as $value) {
-                $my_year[] =$value; // присваиваем массиву значения из $_POST
+    protected function histogramByMonth($collection){
+        $arrayOfParameters =array();
+        $dataYearGist = array();
+        $monthLegend = '';
+        $monthLegend = "'".implode("','",$this->month)."'"; // получаем строку из массива
+        $arrayOfParameters['monthLegend'] = $monthLegend;
+        $myYear = array();// массив с годами
+        if(array_key_exists('myYear',$this->getRequest()->getPost())){ // если существуе в $_POST значение my_year
+            foreach ($_POST['myYear'] as $value) {
+                $myYear[] =$value; // присваиваем массиву значения из $_POST
             }
         }else{
-            $my_year[] =date('Y'); // если нет. то текущая дата.
+            $myYear[] =date('Y'); // если нет. то текущая дата.
         }
-        $count_month = array(); // количество посещений сайта по месяцам
-
-        foreach ($my_year as $k=>$value){
+        $countMonth = array(); // количество посещений сайта по месяцам
+        foreach ($myYear as $k=>$value){
             foreach ($this->month as $key=>$val) {
-                $count_month[$k][$key] = count($collection ->getCollection()-> addFieldToFilter('month',array('eq'=>$key))-> addFieldToFilter('year',array('eq'=>$value)));
+                $countMonth[$k][$key] = count($collection ->getCollection()-> addFieldToFilter('month',array('eq'=>$key))-> addFieldToFilter('year',array('eq'=>$value)));
             }
         }
-        $rgba_year_background_color = '';
-        $rgba_year_border_color = '';
+        $rgbaYearBackgroundColor = '';
+        $rgbaYearBorderColor = '';
         foreach ($this->month as $v) {
-            $rgba_year_background_color = "'rgba(".rand(0,255).",".rand(0,255).",".rand(0,255).",0.5)',".$rgba_year_background_color;// строка с цветовой заливкой
-            $rgba_year_border_color = "'rgba(".rand(0,255).",".rand(0,255).",".rand(0,255).",0.5)',".$rgba_year_border_color;// строка с цветовой заливкой
+            $rgbaYearBackgroundColor = "'rgba(".rand(0,255).",".rand(0,255).",".rand(0,255).",0.5)',".$rgbaYearBackgroundColor;// строка с цветовой заливкой
+            $rgbaYearBorderColor = "'rgba(".rand(0,255).",".rand(0,255).",".rand(0,255).",0.5)',".$rgbaYearBorderColor;// строка с цветовой заливкой
         }
-        foreach ($my_year as $key => $value){
-            $this->dataYearGist[] = "{
+        foreach ($myYear as $key => $value){
+            $dataYearGist[] = "{
             label: 'Посещений в месяц',        
-            data: [".implode(",",$count_month[$key])."],
-            backgroundColor: [".$rgba_year_background_color."],
-            borderColor: [".$rgba_year_border_color."],
+            data: [".implode(",",$countMonth[$key])."],
+            backgroundColor: [".$rgbaYearBackgroundColor."],
+            borderColor: [".$rgbaYearBorderColor."],
             borderWidth: 1},";
         }
+        $arrayOfParameters['dataYearGist'] = $dataYearGist;
+
+        return $arrayOfParameters;
     }
 
-    protected function ChartVisitsByDay($collection){
+    protected function chartVisitsByDay($collection){
+        $arrayOfParameters =array();
         $month_num = array();
-        if(array_key_exists('my_month',$_POST)){
-            foreach ($_POST['my_month'] as $value){
-                $month_num[] = $value;
+        $dayLegend = '';
+        $dayData = array();
+        if(array_key_exists('my_month',$this->getRequest()->getPost())){
+            foreach ($this->getRequest()->getPost('my_month') as $value){
+                $monthNum[] = $value;
             }
         } else {
-            $month_num[] = date('m');
+            $monthNum[] = date('m');
         }
 
-        $year_num = date('Y');
+        $yearNum = date('Y');
         $number = 31; // количество дней в месяце.
         $day = array();
         for ($i = 1; $i <= $number; $i++) {
             $day[] = $i;
         }
-        $this->dayLegend = implode(",",$day); //-------
-        $day_razb = array();
-        foreach ($month_num as $key => $value) {
+        $dayLegend = implode(",",$day); //-------
+        $arrayOfParameters['dayLegend'] = $dayLegend;
+        $dayRazb = array();
+        foreach ($monthNum as $key => $value) {
             foreach ($day as $k => $v) {
-                $day_razb[$key][$k] = count($collection->getCollection()
+                $dayRazb[$key][$k] = count($collection->getCollection()
                     ->addFieldToFilter('day', array('eq' => $v))
-                    ->addFieldToFilter('year', array('eq' => $year_num))
+                    ->addFieldToFilter('year', array('eq' => $yearNum))
                     ->addFieldToFilter('month', array('eq' => $value)));
             }
         }
-        foreach ($day_razb as $key => $value){
-            $this->dayData[] = "{
-                label: '".$this->month[(int)$month_num[$key]]." - ".$year_num."',
+        foreach ($dayRazb as $key => $value){
+            $dayData[] = "{
+                label: '".$this->month[(int)$month_num[$key]]." - ".$yearNum."',
                 data:[".implode(",",$value)."],
                 borderWidth: 1,
                 borderColor: 'rgba(".rand(0,255).",".rand(0,255).",".rand(0,255).",1)',
@@ -117,6 +127,64 @@ class My_Statistic_Block_Adminhtml_Pages extends Mage_Adminhtml_Block_Template{
                 pointBorderWidth: 2,
                 pointRadius: 3},";
         }
+        $arrayOfParameters['dayData'] = $dayData;
+        
+        return $arrayOfParameters;
+    }
+
+    protected function visitsForPeriod($collection){
+        if(array_key_exists('from_data',$this->getRequest()->getPost()) && array_key_exists('to_data',$this->getRequest()->getPost())){
+            $fromData = $this->getRequest()->getPost('from_data');
+            $toData = $this->getRequest()->getPost('to_data');
+        }
+
+        if(!empty($fromData)&& !empty($toData)) {
+            $period = count($collection->getCollection()
+                ->addFieldToFilter('visit_date', array('from' => $fromData, 'to' => $toData)));
+        }else{
+            $period = 'Нет данных';
+        }
+
+        return $period;
+    }
+
+    protected function statistics($collection){
+        $arrayOfParameters =array();
+
+        $year = count($collection -> getCollection()
+            -> addFieldToFilter('year',array('eq'=>date('Y'))));
+        $arrayOfParameters['year'] = $year;
+
+        $month = count($collection -> getCollection()
+            -> addFieldToFilter('month',array('eq'=>date('m')))
+            -> addFieldToFilter('year',array('eq'=>date('Y'))));
+        $arrayOfParameters['month'] = $month;
+
+        $day = count($collection -> getCollection()
+            -> addFieldToFilter('day',array('eq'=>date('d')))
+            -> addFieldToFilter('month',array('eq'=>date('m')))
+            -> addFieldToFilter('year',array('eq'=>date('Y'))));
+        $arrayOfParameters['day'] = $day;
+
+        $hour = count($collection -> getCollection()
+            -> addFieldToFilter('hour',array('eq'=>date('H')))
+            -> addFieldToFilter('day',array('eq'=>date('d')))
+            -> addFieldToFilter('month',array('eq'=>date('m')))
+            -> addFieldToFilter('year',array('eq'=>date('Y'))));
+        $arrayOfParameters['hour'] = $hour;
+
+        $minute = count($collection -> getCollection()
+            -> addFieldToFilter('minute',array('eq'=>date('i')))
+            -> addFieldToFilter('hour',array('eq'=>date('H')))
+            -> addFieldToFilter('day',array('eq'=>date('d')))
+            -> addFieldToFilter('month',array('eq'=>date('m')))
+            -> addFieldToFilter('year',array('eq'=>date('Y'))));
+        $arrayOfParameters['minute'] = $minute;
+
+        $allVisits = count($collection->getCollection());
+        $arrayOfParameters['allVisits'] = $allVisits;
+
+        return $arrayOfParameters;
     }
 
     public function __construct()
@@ -124,31 +192,14 @@ class My_Statistic_Block_Adminhtml_Pages extends Mage_Adminhtml_Block_Template{
         parent::__construct();
         $collection = Mage::getModel('statistic/visits');
         $collection_country = Mage::getModel('statistic/country');
-
-        $this->PieChart($collection_country,'continent');
-        $this->continentData = $this->data;
-        $this->continentLegend = $this->legend;
-        $this->continentRgba = $this->rgba;
-
-        $this->PieChart($collection,'browser_name');
-        $this->browserData = $this->data;
-        $this->browserLegend = $this->legend;
-        $this->browserRgba = $this->rgba;
-
-        $this->PieChart($collection,'sys_fullname');
-        $this->osData = $this->data;
-        $this->osLegend = $this->legend;
-        $this->osRgba = $this->rgba;
-
-        $this->PieChart($collection_country,'country_name_en');
-        $this->countryData = $this->data;
-        $this->countryLegend = $this->legend;
-        $this->countryRgba = $this->rgba;
-        $this->countryDataMaps = $this->countryDataMap;
-
-        $this->HistogramByMonth($collection);
-        $this->ChartVisitsByDay($collection);
-        
+        $this->continentChart = $this->pieChart($collection_country,'continent');
+        $this->browserChart = $this->pieChart($collection,'browser_name');
+        $this->osChart = $this->pieChart($collection,'browser_name');
+        $this->countryChart = $this->pieChart($collection_country,'country_name_en');
+        $this->histogramChart = $this->histogramByMonth($collection);
+        $this->visitsByDayChart = $this->chartVisitsByDay($collection);
+        $this->period = $this->visitsForPeriod($collection);
+        $this->generalStatistics = $this->statistics($collection);
         $this->setTemplate('pages/pages.phtml'); //грузим темплейт (контент который выводим) из design/adminhtml/default/default/template/pages/test.phtml
     }
 
--- app/code/local/My/Statistic/controllers/Adminhtml/PagesController.php
+++ app/code/local/My/Statistic/controllers/Adminhtml/PagesController.php
@@ -1,65 +1,9 @@
 <?php
 
 class My_Statistic_Adminhtml_PagesController extends Mage_Adminhtml_Controller_Action {
-   // public $a;
 
     public function indexAction()
     {
-        date_default_timezone_set('Europe/Moscow'); // установим временную зону
-        $collection = Mage::getModel('statistic/visits');
-        $collection_country = Mage::getModel('statistic/country');
-
-        //
-        //
-        // Посещения за периуд
-        //
-        //
-        if(array_key_exists('from_data',$_POST) && array_key_exists('to_data',$_POST)){
-            $from_data = $_POST['from_data'];
-            $to_data = $_POST['to_data'];
-        }
-
-        if(!empty($from_data)&& !empty($to_data)) {
-            $period = count($collection->getCollection()
-                ->addFieldToFilter('visit_date', array('from' => $from_data, 'to' => $to_data)));
-        }else{
-            $period = 'Нет данных';
-        }
-
-        $year = count($collection -> getCollection()
-                                  -> addFieldToFilter('year',array('eq'=>date('Y'))));
-
-        $month = count($collection -> getCollection()
-                                   -> addFieldToFilter('month',array('eq'=>date('m')))
-                                   -> addFieldToFilter('year',array('eq'=>date('Y'))));
-
-        $day = count($collection -> getCollection()
-                                 -> addFieldToFilter('day',array('eq'=>date('d')))
-                                 -> addFieldToFilter('month',array('eq'=>date('m')))
-                                 -> addFieldToFilter('year',array('eq'=>date('Y'))));
-
-        $hour = count($collection -> getCollection()
-                                  -> addFieldToFilter('hour',array('eq'=>date('H')))
-                                  -> addFieldToFilter('day',array('eq'=>date('d')))
-                                  -> addFieldToFilter('month',array('eq'=>date('m')))
-                                  -> addFieldToFilter('year',array('eq'=>date('Y'))));
-
-        $minute = count($collection -> getCollection()
-                                    -> addFieldToFilter('minute',array('eq'=>date('i')))
-                                    -> addFieldToFilter('hour',array('eq'=>date('H')))
-                                    -> addFieldToFilter('day',array('eq'=>date('d')))
-                                    -> addFieldToFilter('month',array('eq'=>date('m')))
-                                    -> addFieldToFilter('year',array('eq'=>date('Y'))));
-
-        $allvisits = count($collection->getCollection());
-        Mage::register('allvisits', $allvisits);
-        Mage::register('year', $year);
-        Mage::register('month', $month);
-        Mage::register('day', $day);
-        Mage::register('hour', $hour);
-        Mage::register('minute', $minute);
-        Mage::register('period', $period);
-
         // создаем свой блок для вывода
         $this->loadLayout();
         $this->_addContent($this->getLayout()->createBlock('statistic/adminhtml_pages'));// Statistic/Block(пропускаеться)/Adminhtml/Pages
